bff39da28f94d3bedf87fd4a3885393ab3c3add3
fjhanevo <fredrik.hanevold@gmail.com>
2025-09-21 16:52:51 +0200

    back to drawing squares, preparing for texture drawing

--- space_game/src/main.cpp
+++ space_game/src/main.cpp
@@ -43,26 +43,31 @@ int main()
     // ------- Vertex setup -------
     float vertices[] = {
         // positions        // colors
-        -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,    // bottom right
+        0.5f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f,    // bottom right
         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,    // bottom left
-        0.0f, 0.5f, 0.0f,   0.0f, 0.0f, 1.0f     // top
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,     // top
+        -0.5f, 0.5f, 0.0f,  1.0f, 1.0f, 0.0f
     };
 
-    float text_coords[] = {
-        0.0f, 0.0f, // lower left corner
-        1.0f, 0.0f, // lower right cornes
-        0.5f, 1.0f, // top-center corner
+    unsigned int indices[] = {
+        0,1,3,
+        1,2,3
     };
 
 
     unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
@@ -82,7 +87,7 @@ int main()
         //activate shader
         shader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // swap buffers and poll IO events
         glfwSwapBuffers(window);

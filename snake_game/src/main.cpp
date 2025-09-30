cee13b65dc6072376bf8b3ce255b66c2dd261c2e
fjhanevo <fredrik.hanevold@gmail.com>
2025-09-30 19:50:17 +0200

    moved all logic to game.cpp

--- snake_game/src/main.cpp
+++ snake_game/src/main.cpp
@@ -1,62 +1,9 @@
-#include <iostream>
+#include "game.h"
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-const unsigned int SCREEN_WIDTH = 800;
-const unsigned int SCREEN_HEIGHT = 600;
-
-// callback functions
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
 
 int main()
 {
-    // let's test if we can draw an emtpy window to the screen
-    glfwInit();
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Test", NULL, NULL);
-    if (window == NULL) {
-        std::cout << "Failed to create GLFW Window\n";
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-    // set callback functions
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetKeyCallback(window, key_callback);
-
-    // load openGL function pointers
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD\n";
-        return -1;
-    }
-
-    // render loop
-    while (!glfwWindowShouldClose(window)) {
-        // draw a temp background
-        glClearColor(0.2f, 0.6f, 0.2f,1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+    Game game(800,600);
+    game.run();
     return 0;
-}
-
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS || 
-        key == GLFW_KEY_Q && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
 }

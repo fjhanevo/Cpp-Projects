84256fc1f50e70b9285c82fe21a940cae20aa1ac
fjhanevo <fredrik.hanevold@gmail.com>
2025-09-30 19:50:26 +0200

    added game.h

---
+++ snake_game/include/game.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+class Game {
+public:
+    Game(unsigned int width, unsigned int height);
+    ~Game();
+
+    void run();
+private:
+    GLFWwindow *window;
+    unsigned int screen_width, screen_height;
+    void init();
+    void cleanup();
+    void render();
+    void process_input();
+    
+    // callback wrappers
+    static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
+    static void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+};

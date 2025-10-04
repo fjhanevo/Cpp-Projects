04cff4f08e776cf8f5205c6824ff39d1d6b2095e
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-04 20:19:59 +0200

    added more to cleanup and init

--- snake_game/src/game.cpp
+++ snake_game/src/game.cpp
@@ -1,11 +1,18 @@
 #include "game.h"
 #include "shader.h"
+#include "sprite_renderer.h"
 #include "texture.h"
 #include "resource_manager.h"
 #include <GLFW/glfw3.h>
+#include "config.h"
 #include <iostream>
 
+#include <glm/gtc/matrix_transform.hpp>
 
+SpriteRenderer *Renderer;
+std::string vert_path = std::string(RESOURCE_DIR) + "/shaders/sprite.vert";
+std::string frag_path = std::string(RESOURCE_DIR) + "/shaders/sprite.frag";
+std::string texture_path = std::string(RESOURCE_DIR) + "/textures/sofie.png";
 
 Game::Game(unsigned int width, unsigned int height)
 : screen_width(width), screen_height(height), window(nullptr), state(GAME_ACTIVE)
@@ -42,12 +49,26 @@ void Game::init()
     glfwSetKeyCallback(window, key_callback);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+
+
     // load openGL function pointers
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD\n";
         return;
     }
-    // glViewport(0, 0, this->screen_width, this->screen_height);
+
+    // load shaders
+    ResourceManager::load_shader(vert_path.c_str(), frag_path.c_str(), "sprite");
+    // configure shaders
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->screen_width),
+                           static_cast<float>(this->screen_height), 0.0f, -1.0f, 1.0f);
+    ResourceManager::get_shader("sprite").use().set_int("image", 0);
+    ResourceManager::get_shader("sprite").set_mat4("projection", projection);
+
+    Shader shader = ResourceManager::get_shader("sprite");
+    Renderer = new SpriteRenderer(shader);
+    ResourceManager::load_texture(texture_path.c_str(), true, "sofie");
+
 }
 
 void Game::run()
@@ -65,14 +86,20 @@ void Game::process_input()
 
 void Game::render()
 {
-    glClearColor(0.2f, 0.6f, 0.2f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+    Texture2D tex = ResourceManager::get_texture("sofie");
+    Renderer->draw_sprite(tex,
+        glm::vec2(200.f, 200.f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
     glfwSwapBuffers(window);
 }
 
 void Game::cleanup()
 {
+    delete Renderer;
+    ResourceManager::clear();
     glfwTerminate();
+
 }
 
 

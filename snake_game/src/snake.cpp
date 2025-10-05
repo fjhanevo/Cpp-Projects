6482ba69de77b3ce41fd29aa79df331ac0293ec2
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-05 17:22:31 +0200

    added snake.cpp

---
+++ snake_game/src/snake.cpp
@@ -0,0 +1,57 @@
+#include "snake.h"
+#include "resource_manager.h"
+#include "sprite_renderer.h"
+
+Snake::Snake(unsigned int screen_width, unsigned int screen_height)
+{
+    this->current_direction = Direction::RIGHT;
+
+    this->segments.clear();
+
+    // create snake in the centre of the screen
+    glm::vec2 snake_head = { screen_width / 2.0f, screen_height / 2.0f };
+    this->segments.push_back(snake_head);
+
+    // add some more segments just to test
+    this->segments.push_back({snake_head.x, snake_head.y- 20.0f});
+    this->segments.push_back({snake_head.x, snake_head.y- 40.0f});
+
+}
+
+
+void Snake::draw(SpriteRenderer &renderer)
+{
+    Texture2D snake_head_tex = ResourceManager::get_texture("temp");
+    Texture2D snake_body_tex = ResourceManager::get_texture("temp");
+    
+    glm::vec3 snake_head_color = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 snake_body_color = glm::vec3(0.2f, 0.2f, 0.2f);
+    float tile_size = 20.0f;
+    glm::vec2 snake_size = glm::vec2(tile_size, tile_size);
+
+    if (!segments.empty())
+    {
+        // draw head first
+        renderer.draw_sprite(
+            snake_head_tex, 
+            segments[0],
+            snake_size,
+            0.0f,   // TODO: Make head rotate based on Direction
+            snake_head_color
+        );
+
+        // draw rest of the body
+        for (size_t i = 1; i < segments.size(); ++i)
+        {
+            renderer.draw_sprite(
+                snake_body_tex,
+                segments[i],
+                snake_size,
+                0.0f,
+                snake_body_color
+            );
+        }
+    }
+
+    
+}

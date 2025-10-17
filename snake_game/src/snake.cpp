17bf94aa53e8e8d03f752752e8f2fac2eb506c95
fjhanevo <fredrik.hanevold@gmail.com>
2025-10-17 22:28:38 +0200

    added snake_size to the constructor

--- snake_game/src/snake.cpp
+++ snake_game/src/snake.cpp
@@ -3,8 +3,9 @@
 #include "sprite_renderer.h"
 #include "config.h"
 
-Snake::Snake(unsigned int screen_width, unsigned int screen_height)
-: current_direction(Direction::DOWN), next_direction(Direction::DOWN)
+Snake::Snake(unsigned int screen_width, unsigned int screen_height) :
+current_direction(Direction::DOWN), next_direction(Direction::DOWN),
+snake_size(glm::vec2(TILE_SIZE, TILE_SIZE))
 {
 
     this->segments.clear();
@@ -19,23 +20,22 @@ Snake::Snake(unsigned int screen_width, unsigned int screen_height)
 
 void Snake::draw(SpriteRenderer &renderer)
 {
-    Texture2D snake_head_tex = ResourceManager::get_texture("temp");
-    Texture2D snake_body_tex = ResourceManager::get_texture("temp");
-    Texture2D snake_tail_tex = ResourceManager::get_texture("temp");
+    Texture2D head_tex = ResourceManager::get_texture("temp");
+    Texture2D body_tex = ResourceManager::get_texture("temp");
+    Texture2D tail_tex = ResourceManager::get_texture("temp");
     
     glm::vec3 snake_head_color = glm::vec3(0.0f, 0.0f, 0.0f);
     glm::vec3 snake_body_color = glm::vec3(0.2f, 0.2f, 0.2f);
     glm::vec3 snake_tail_color = glm::vec3(0.8f, 0.2f, 0.2f);
-    glm::vec2 snake_size = glm::vec2(TILE_SIZE, TILE_SIZE);
 
     if (segments.empty())
         return;
     
     // draw head first
     renderer.draw_sprite(
-        snake_head_tex, 
+        head_tex, 
         segments[0],
-        snake_size,
+        this->snake_size,
         0.0f,   // TODO: Make head rotate based on Direction
         snake_head_color
     );
@@ -44,7 +44,7 @@ void Snake::draw(SpriteRenderer &renderer)
     for (size_t i = 1; i < segments.size() - 1; ++i)
     {
         renderer.draw_sprite(
-            snake_body_tex,
+            body_tex,
             segments[i],
             snake_size,
             0.0f,
@@ -54,7 +54,7 @@ void Snake::draw(SpriteRenderer &renderer)
     // draw the tail
     if (this->segments.size() > 1)
         renderer.draw_sprite(
-            snake_tail_tex,
+            tail_tex,
             segments[segments.size()-1],
             snake_size,
             0.0f,
